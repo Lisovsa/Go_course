de4dab79e31942c05fc26954af62e5436d8ed82b
Lisovsa <muen2@mail.ru>
2018-12-05 19:31:07 +0300

    Possible features

--- first/main.go
+++ first/main.go
@@ -1,8 +1,11 @@
 package main
 import "fmt"
+import f "fmt"
 
 func main() {
   var hello = "Hello,"
   fmt.Println(hello, "Lena!")
+  f.Println(hello)
   bye()
+  fmt.Println("inanc"); fmt.Println("lina"); fmt.Println("ebru");
 }
